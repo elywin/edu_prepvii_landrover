5987f0c39ab5fd1c554b6af957ff1c76433a670e
elywin <lukeeraelywin@gmail.com>
2020-09-03 15:26:20 +0300

    ft(add:created userSignupRoute)

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,5 +6,8 @@ const router = express.Router();
 const User = require("../models/user");
 
 router.post("/signup", (req, res, next) => {
-
+    const user = new User({
+        email:req.body.email,
+        password:req.body.password
+    });
 });
