2debf2bc9afb14f9b3989f6fcefdfbd02cda490c
elywin <lukeeraelywin@gmail.com>
2020-09-03 16:20:02 +0300

    ft(add:add password encryption)

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,12 +2,35 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const User = require("../models/user");
 
 router.post("/signup", (req, res, next) => {
-    const user = new User({
-        email:req.body.email,
-        password:req.body.password
+    bcrypt.hash(req.body.password, 10, (err,hash)=>{
+        if(err){
+            return res.status(500).json({
+                error:err
+            });
+
+        }else{
+            const user = new User({
+                email:req.body.email,
+                password:hash
+                });
+                user.save().then(result =>{
+                    console.log(result);
+                    res.status(201).json({
+                        message:'user created successfully'
+                    });
+               
+            }).catch(err =>{
+                console.log(err);
+                res.status(500).json({
+                    error:err
+                });
+            });
+        }
+   
     });
 });
