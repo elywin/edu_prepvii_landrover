ff29e6f16204fd981749efaf25668aa5c4a2d4c0
elywin <lukeeraelywin@gmail.com>
2020-09-04 19:13:13 +0300

    ft(add:eslint prettier code format)[#174625222]

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //database connection
-const url = 'mongodb://localhost/api_db';
-mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true });
+const url = "mongodb://localhost/api_db";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 //
-app.use(express.json()); 
+app.use(express.json());
 
 //posts middleware
 app.use("/posts", routes);
 
-
-app.listen(port,()=>{
-    console.log(`server is listening on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`server is listening on http://localhost:${port}`);
 });
