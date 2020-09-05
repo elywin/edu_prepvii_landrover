e194c258d6b414afe0c375107b51bc559899f258
elywin <lukeeraelywin@gmail.com>
2020-09-05 12:06:04 +0300

    ft(add:refactor code)

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,1 +1,51 @@
 const User = require("../models/schema/schema");
+const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const express = require("express");
+
+//create user using post method
+module.exports.signup_post= (req, res, next) => {
+    //get the user from the user model
+    User.find({
+            email: req.body.email
+        })
+        .exec()
+        //get the user email check if it exists
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "email exists"
+                });
+            } else {
+                //password ecnryption with bcrypt
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+
+                    } else {
+                        const user = new User({
+                            email: req.body.email,
+                            password: hash
+                        });
+                        //save user created to database
+                        user.save().then(result => {
+                            console.log(result);
+                            res.status(201).json({
+                                message: 'user created successfully'
+                            });
+
+                        }).catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            });
+                        });
+                    }
+
+                });
+            }
+        });
+
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,54 +1,8 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const User = require("../models/user"); //import user model
+const controlRoute = require("../controllers/user");
 
-//create user using post method
-router.post("/signup", (req, res, next) => {
-    //get the user from the user model
-    User.find({
-            email: req.body.email
-        })
-        .exec()
-        //get the user email check if it exists
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: "email exists"
-                });
-            } else {
-                //password ecnryption with bcrypt
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        });
 
-                    } else {
-                        const user = new User({
-                            email: req.body.email,
-                            password: hash
-                        });
-                        //save user created to database
-                        user.save().then(result => {
-                            console.log(result);
-                            res.status(201).json({
-                                message: 'user created successfully'
-                            });
-
-                        }).catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err
-                            });
-                        });
-                    }
-
-                });
-            }
-        });
-
-});
+router.post('/signup',controlRoute.signup_post);
 
 module.exports = router;
