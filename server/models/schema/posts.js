baf1883c4682076b09a49e45446dd519a89be297
elywin <lukeeraelywin@gmail.com>
2020-09-06 03:05:34 +0300

    ft(add:track user post)

--- server/models/schema/posts.js
+++ server/models/schema/posts.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema({
   title: String,
   content: String,
+  user:{type:mongoose.Schema.Types.ObjectId,
+  ref:'User'
+  }
 });
 
 module.exports = mongoose.model("post", schema);
