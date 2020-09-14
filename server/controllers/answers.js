e399967a832e72706fc077e531e4d53356374582
elywin <lukeeraelywin@gmail.com>
2020-09-14 14:10:56 +0300

    ch(rebase complete)

--- server/controllers/answers.js
+++ server/controllers/answers.js
@@ -22,31 +22,6 @@ module.exports.ans_get = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-      );
-      res.json({answer:answer});
-    } catch (error) {
-     res.json({message:"No Answers found!!"}).status(404);
-    }
-   
-  };
-
-
-  // Post an answer to a question
-module.exports.post_ans = async (req, res) => {
-  let question_id = req.params.id;
-  
-  let newAnswer = new ans(
-    {
-     
-      answer:req.body.answer,
-      user:req.params.id,
-    }
-  )
-  
-   newAnswer.save((err) => {
-    if (err) return console.log(`**ERROR** saving answer: ${err}`)
-=======
 // Post an answer to a question
 module.exports.post_ans = async (req, res) => {
   let question_id = req.params.id;
@@ -59,49 +34,10 @@ module.exports.post_ans = async (req, res) => {
 
   newAnswer.save((err) => {
     if (err) return console.log(`**ERROR** saving answer: ${err}`);
->>>>>>> ft(add:accepted answer)[Finishes #174625342]
     Question.findOneAndUpdate(
       {
         _id: question_id,
       },
-<<<<<<< HEAD
-      { $push: { "answer.0": newAnswer }},
-      { new: true}
-    ).then( question=> {
-      res.status(200).json(question);
-      
-    }).catch(err => {
-      res.status(500).json({ failedToUpdate: "Failed to save the answer!"})
-    })
-  })
- 
-}
-
-
-// // Accept a preferred answer
-// module.exports.acceptAnswer = async (req, res) => {
-//   const question_id =  req.params.question_id; 
-//   const answer_id =  req.params.answer_id ;
-
-//   console.log(`>> question_id : ${question_id}`)
-//   console.log(`>> answer_id : ${answer_id}`)
-
-//   Question.findOneAndUpdate(
-//     { 
-//       _id: question_id,
-//      // user: req.params.id,
-//     },
-//     { "acceptedAnswer": { id: answer_id }},
-//     { new: true },
-//     (err, question) => {
-//       if (err) throw console.log(`**ERROR** : ${err}`)
-//       res.status(200).json(question )
-//     }
-    
-//   )
-//   console.log(`>> answer_id : ${answer_id}`)
-// }
-=======
       {
         $push: {
           "answer.0": newAnswer,
@@ -122,7 +58,6 @@ module.exports.post_ans = async (req, res) => {
       });
   });
 };
->>>>>>> ft(add:accepted answer)[Finishes #174625342]
 
 // Accept a preferred answer
 module.exports.acceptAnswer = async (req, res) => {
